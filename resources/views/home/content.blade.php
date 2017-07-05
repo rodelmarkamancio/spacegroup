d655b54256c68b00f1777703dc5930d65ee43f6e
Rodel Amancio <rodel.amancio@spacegroup.co.uk>
2017-07-05 16:27:54 +0100

    completed the index page for the new website of space group

--- resources/views/home/content.blade.php
+++ resources/views/home/content.blade.php
@@ -1,45 +1,103 @@
 <div class="space-home-content">
-    <div class="container">
-        <div class="row content-row">
+    <div class="space--content-wrapper">
+        <div class="container">
+            <div class="row content-row">
 
-            <div class="col-md-4">
-                <div class="content-image-wrap">
-                    <a href="#"><img src="http://via.placeholder.com/300?text=Space+Group" alt=""></a>
+                <div class="col-md-4 content-detail-wrapper">
+                    <div class="content-image-wrap">
+                        <a href="#"><img src="http://spacegroup.co.uk/files/node_element_content/image_url/271/Granary%20Square%202280%20x%201280.jpg" alt=""></a>
+                    </div>
+                    <div class="content-title">
+                        <a href="#">Designing</a>
+                    </div>
+                    <div class="content-description">
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                    </div>
                 </div>
-                <div class="content-info">
-                    <label for="" class="content-category">Lorem Ipsum</label>
+
+                <div class="col-md-4 content-detail-wrapper">
+                    <div class="content-image-wrap">
+                        <a href="#"><img src="http://spacegroup.co.uk/files/node_element_content/image_url/268/Civic%20Theatre%20View%20on%20Parkgate%202280%20x%201280.jpg" alt=""></a>
+                    </div>
+                    <div class="content-title">
+                        <a href="#">Innovating</a>
+                    </div>
+                    <div class="content-description">
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at magna</h3>
+                    </div>
                 </div>
-                <div class="content-title">
-                    <h2><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h2>
+
+                <div class="col-md-4 content-detail-wrapper">
+                    <div class="content-image-wrap">
+                        <a href="#"><img src="http://spacegroup.co.uk/files/node_element_content/image_url/259/NodeElementContent_5926dfe2ef524.jpeg" alt=""></a>
+                    </div>
+                    <div class="content-title">
+                        <a href="#">Connecting</a>
+                    </div>
+                    <div class="content-description">
+                        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                    </div>
                 </div>
+
             </div>
-
-            <div class="col-md-4">
-                <div class="content-image-wrap">
-                    <a href="#"><img src="http://via.placeholder.com/300?text=Space+Group" alt=""></a>
-                </div>
-                <div class="content-info">
-                    <label for="" class="content-category">Lorem Ipsum</label>
-                    <label for="" class="content-author">Space Group</label>
-                </div>
-                <div class="content-title">
-                    <h2><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h2>
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <div class="content-image-wrap">
-                    <a href="#"><img src="http://via.placeholder.com/300?text=Space+Group" alt=""></a>
-                </div>
-                <div class="content-info">
-                    <label for="" class="content-category">Lorem Ipsum</label>
-                    <label for="" class="content-author">Space Group</label>
-                </div>
-                <div class="content-title">
-                    <h2><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h2>
-                </div>
-            </div>
-
         </div>
     </div>
+
+    <div class="space--middle-content-wrapper">
+        <div class="container">
+            <div class="row content-row">
+                <div class="col-md-6 content-detail"></div>
+                <div class="col-md-6 content-detail">
+                    <div class="content-info">
+                        <h2>We connect with people & companies who share our drive for doing things better.</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="space--middle-intro-wrapper">
+        <div class="container">
+            <div class="row content-row">
+                <div class="col-md-12 content-detail-title">
+                    <h2>As we enter the fourth industrial revolution, new thinking, combined with emerging technologies, is encouraging unprecedented innovation & challenge across construction & property.</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="space--news-content">
+        <div class="container">
+            <div class="row content-row">
+                <div class="col-md-4 content-detail-wrapper">
+                    <div class="content-info">
+                        <label for="" class="content-category">News</label>
+                    </div>
+                    <div class="content-title">
+                        <h2><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h2>
+                    </div>
+                    <div class="content-link"><a href="#" class="link">Find out more</a></div>
+                </div>
+                <div class="col-md-4 content-detail-wrapper">
+                    <div class="content-info">
+                        <label for="" class="content-category">News</label>
+                    </div>
+                    <div class="content-title">
+                        <h2><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h2>
+                    </div>
+                    <div class="content-link"><a href="#" class="link">Find out more</a></div>
+                </div>
+                <div class="col-md-4 content-detail-wrapper">
+                    <div class="content-info">
+                        <label for="" class="content-category">News</label>
+                    </div>
+                    <div class="content-title">
+                        <h2><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h2>
+                    </div>
+                    <div class="content-link"><a href="#" class="link">Find out more</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,14 @@
+<div class="footer">
+    <div class="footer-wrapper">
+        <div class="footer-content">
+            <h3>Network</h3>
+            <ul>
+                <li><a href="#">Space Architects</a></li>
+                <li><a href="#">BIMstore</a></li>
+                <li><a href="#">BIMtechnologies</a></li>
+                <li><a href="#">Space Technologies</a></li>
+                <li><a href="#">BIM Show Live</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
